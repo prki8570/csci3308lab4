9f4a4b743c35acc7d5651a7d68520619e41f9191
prki8570 <prki8570@colorado.edu>
2014-09-17 13:44:51 -0600

    Update hello.java
    added test

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("How are you?");
+		System.out.println("Hello Laxit!");
 
 	}
 
